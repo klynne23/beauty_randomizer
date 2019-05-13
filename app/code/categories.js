2c74547701d852dcc6142a272ab004458ab51801
Kirthi Bisset <kirthi.bisset@yahoo.com>
2019-05-13 19:26:58 -0400

    erased saved data

--- app/code/categories.js
+++ app/code/categories.js
@@ -1,533 +1,62 @@
 let categories = [
     {
         category: 'moisturizer',
-        items: [
-            {
-                id: 2384.230492342,
-                brand: 'Sephora',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Small',
-                category: 'moisturizer'
-            },
-            {
-                id: 2384.23423432,
-                brand: 'NYX',
-                quality: 'Low End',
-                color: 'White',
-                size: 'Large',
-                category: 'moisturizer'
-            },
-            {
-                id: 2384.4589439085,
-                brand: 'Morphe',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Medium',
-                category: 'moisturizer'
-            },
-            {
-                id: 2384.3453453456,
-                brand: 'Elf',
-                quality: 'Drugstore',
-                color: 'Nude',
-                size: 'Large',
-                category: 'moisturizer'
-            },
-        ]
+        items: []
     },
     {
         category: 'eye primer',
-        items: [
-            {
-                id: 2384.87546758,
-                brand: 'Sephora',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Small',
-                category: 'eye primer'
-            },
-            {
-                id: 2384.678456356,
-                brand: 'NYX',
-                quality: 'Low End',
-                color: 'White',
-                size: 'Large',
-                category: 'eye primer'
-            },
-            {
-                id: 2384.67967364,
-                brand: 'Morphe',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Medium',
-                category: 'eye primer'
-            },
-            {
-                id: 2384.5856745334,
-                brand: 'Elf',
-                quality: 'Drugstore',
-                color: 'Nude',
-                size: 'Large',
-                category: 'eye primer'
-            },
-        ]
+        items: []
     },
     {
         category: 'face primer',
-        items: [
-            {
-                id: 2384.2395386454,
-                brand: 'Sephora',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Small',
-                category: 'face primer'
-            },
-            {
-                id: 2384.30486304,
-                brand: 'NYX',
-                quality: 'Low End',
-                color: 'White',
-                size: 'Large',
-                category: 'face primer'
-            },
-            {
-                id: 2384.20384346,
-                brand: 'Morphe',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Medium',
-                category: 'face primer'
-            },
-            {
-                id: 2384.09864956,
-                brand: 'Elf',
-                quality: 'Drugstore',
-                color: 'Nude',
-                size: 'Large',
-                category: 'face primer'
-            },
-
-        ]
+        items: []
 
     },
     {
         category: 'face powder',
-        items: [
-            {
-                id: 2384.45645856,
-                brand: 'Sephora',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Small',
-                category: 'face powder'
-            },
-            {
-                id: 2384.045790456,
-                brand: 'NYX',
-                quality: 'Low End',
-                color: 'White',
-                size: 'Large',
-                category: 'face powder'
-            },
-            {
-                id: 2384.12948234,
-                brand: 'Morphe',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Medium',
-                category: 'face powder'
-            },
-            {
-                id: 2384.9368039485,
-                brand: 'Elf',
-                quality: 'Drugstore',
-                color: 'Nude',
-                size: 'Large',
-                category: 'face powder'
-            },
-
-        ]
+        items: []
 
     },
     {
         category: 'setting spray',
-        items: [
-            {
-                id: 2384.86543245,
-                brand: 'Sephora',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Small',
-                category: 'setting spray'
-            },
-            {
-                id: 2384.23456434,
-                brand: 'NYX',
-                quality: 'Low End',
-                color: 'White',
-                size: 'Large',
-                category: 'setting spray'
-            },
-            {
-                id: 2384.45676543,
-                brand: 'Morphe',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Medium',
-                category: 'setting spray'
-            },
-            {
-                id: 2384.23456754,
-                brand: 'Elf',
-                quality: 'Drugstore',
-                color: 'Nude',
-                size: 'Large',
-                category: 'setting spray'
-            },
-
-        ]
+        items: []
 
     },
     {
         category: 'highlighter',
-        items: [
-            {
-                id: 2384.876543,
-                brand: 'Sephora',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Small',
-                category: 'highlighter'
-            },
-            {
-                id: 2384.23456654,
-                brand: 'NYX',
-                quality: 'Low End',
-                color: 'White',
-                size: 'Large',
-                category: 'highlighter'
-            },
-            {
-                id: 2384.56787654,
-                brand: 'Morphe',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Medium',
-                category: 'highlighter'
-            },
-            {
-                id: 2384.347454345,
-                brand: 'Elf',
-                quality: 'Drugstore',
-                color: 'Nude',
-                size: 'Large',
-                category: 'highlighter'
-            },
-
-        ]
+        items: []
     },
     {
         category: 'blush',
-        items: [
-            {
-                id: 2384.86543,
-                brand: 'Sephora',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Small',
-                category: 'blush'
-            },
-            {
-                id: 3464.23423432,
-                brand: 'NYX',
-                quality: 'Low End',
-                color: 'White',
-                size: 'Large',
-                category: 'blush'
-            },
-            {
-                id: 2356.4589439085,
-                brand: 'Morphe',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Medium',
-                category: 'blush'
-            },
-            {
-                id: 8765.3453453456,
-                brand: 'Elf',
-                quality: 'Drugstore',
-                color: 'Nude',
-                size: 'Large',
-                category: 'blush'
-            },
-
-        ]
+        items: []
     },
     {
         category: 'contour',
-        items: [
-            {
-                id: 6834.230492342,
-                brand: 'Sephora',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Small',
-                category: 'contour'
-            },
-            {
-                id: 2384.2334534423432,
-                brand: 'NYX',
-                quality: 'Low End',
-                color: 'White',
-                size: 'Large',
-                category: 'contour'
-            },
-            {
-                id: 2384.4589534743439085,
-                brand: 'Morphe',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Medium',
-                category: 'contour'
-            },
-            {
-                id: 345.3453453456,
-                brand: 'Elf',
-                quality: 'Drugstore',
-                color: 'Nude',
-                size: 'Large',
-                category: 'contour'
-            },
-
-        ]
+        items: []
     },
     {
         category: 'eye shadow',
-        items: [
-            {
-                id: 23345384.230492342,
-                brand: 'Sephora',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Small',
-                category: 'eye shadow'
-            },
-            {
-                id: 2384.876347,
-                brand: 'NYX',
-                quality: 'Low End',
-                color: 'White',
-                size: 'Large',
-                category: 'eye shadow'
-            },
-            {
-                id: 2384.68897656,
-                brand: 'Morphe',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Medium',
-                category: 'eye shadow'
-            },
-            {
-                id: 2384.347765434,
-                brand: 'Elf',
-                quality: 'Drugstore',
-                color: 'Nude',
-                size: 'Large',
-                category: 'eye shadow'
-            },
-
-        ]
+        items: []
     },
     {
         category: 'liquid eye liner',
-        items: [
-            {
-                id: 2384.693247654,
-                brand: 'Sephora',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Small',
-                category: 'liquid eye liner'
-            },
-            {
-                id: 7654.23423432,
-                brand: 'NYX',
-                quality: 'Low End',
-                color: 'White',
-                size: 'Large',
-                category: 'liquid eye liner'
-            },
-            {
-                id: 2384.3467898,
-                brand: 'Morphe',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Medium',
-                category: 'liquid eye liner'
-            },
-            {
-                id: 3453.3453453456,
-                brand: 'Elf',
-                quality: 'Drugstore',
-                color: 'Nude',
-                size: 'Large',
-                category: 'liquid eye liner'
-            },
-
-        ]
+        items: []
     },
     {
         category: 'kohl eye liner',
-        items: [
-            {
-                id: 2384.2304956852342,
-                brand: 'Sephora',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Small',
-                category: 'kohl eye liner'
-            },
-            {
-                id: 2345384.23423432,
-                brand: 'NYX',
-                quality: 'Low End',
-                color: 'White',
-                size: 'Large',
-                category: 'kohl eye liner'
-            },
-            {
-                id: 2384.3478543,
-                brand: 'Morphe',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Medium',
-                category: 'kohl eye liner'
-            },
-            {
-                id: 345.3453453456,
-                brand: 'Elf',
-                quality: 'Drugstore',
-                color: 'Nude',
-                size: 'Large',
-                category: 'kohl eye liner'
-            },
-
-        ]
+        items: []
     },
     {
         category: 'mascara',
-        items: [{
-            id: 2384.65757,
-            brand: 'Sephora',
-            quality: 'High End',
-            color: 'Pink',
-            size: 'Small',
-            category: 'mascara'
-        },
-        {
-            id: 3424.23423432,
-            brand: 'NYX',
-            quality: 'Low End',
-            color: 'White',
-            size: 'Large',
-            category: 'mascara'
-        },
-        {
-            id: 2384.567565,
-            brand: 'Morphe',
-            quality: 'High End',
-            color: 'Pink',
-            size: 'Medium',
-            category: 'mascara'
-        },
-        {
-            id: 2384.89756756,
-            brand: 'Elf',
-            quality: 'Drugstore',
-            color: 'Nude',
-            size: 'Large',
-            category: 'mascara'
-        },
-        ]
+        items: []
     },
     {
         category: 'lip liner',
-        items: [
-            {
-                id: 2384.34534,
-                brand: 'Sephora',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Small',
-                category: 'lip liner'
-            },
-            {
-                id: 2384.23423467432,
-                brand: 'NYX',
-                quality: 'Low End',
-                color: 'White',
-                size: 'Large',
-                category: 'lip liner'
-            },
-            {
-                id: 2384.45645457,
-                brand: 'Morphe',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Medium',
-                category: 'lip liner'
-            },
-            {
-                id: 2384.4564564,
-                brand: 'Elf',
-                quality: 'Drugstore',
-                color: 'Nude',
-                size: 'Large',
-                category: 'lip liner'
-            },
-
-        ]
+        items: []
     },
     {
         category: 'lipstick',
-        items: [
-            {
-                id: 2384.2304923345342,
-                brand: 'Sephora',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Small',
-                category: 'lipstick'
-            },
-            {
-                id: 3765.23423432,
-                brand: 'NYX',
-                quality: 'Low End',
-                color: 'White',
-                size: 'Large',
-                category: 'lipstick'
-            },
-            {
-                id: 98979.4589439085,
-                brand: 'Morphe',
-                quality: 'High End',
-                color: 'Pink',
-                size: 'Medium',
-                category: 'lipstick'
-            },
-            {
-                id: 78973.3453453456,
-                brand: 'Elf',
-                quality: 'Drugstore',
-                color: 'Nude',
-                size: 'Large',
-                category: 'lipstick'
-            },
-        ]
+        items: []
     },
 ];
 
